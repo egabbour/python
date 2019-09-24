41cebb33586a4b31b8478465818e5bc8802c8722
user <egabbour@isfpegypt.com>
2019-09-24 12:14:34 +0200

    1

---
+++ 123.py
@@ -0,0 +1,5 @@
+
+from datetime import datetime
+now = datetime.now()
+today = now.strftime("%d/%m/%Y")
+print("TechCampus Date is", today, "🎉 and we are updating our Cloud for you 🚀 Please wait 🙌")
